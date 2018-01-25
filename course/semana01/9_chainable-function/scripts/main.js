61e3eaefc25cbbffd2f4a33506f580000669313e
VGamezz19 <victor.gamez1596@gmail.com>
2018-01-25 09:30:28 +0100

    chainable-function done

--- course/semana01/9_chainable-function/scripts/main.js
+++ course/semana01/9_chainable-function/scripts/main.js
@@ -10,25 +10,44 @@ function test(_word) {
     }
 
     return {
-        wrap(_firstSymbol, _secondSymbol) {
-            var firstSymbol = _firstSymbol || false;
-            var secondSymbol = _secondSymbol || false;
+        wrap: function wrap(inputFirstSymbol, inputSecondSymbol) {
+            var firstSymbol = inputFirstSymbol || "";
+            var secondSymbol = inputSecondSymbol || "";
+            var regExp = /[^\d\w\sñç]/g;
+
             try {
+                //ThrowError if...
+                if (typeof (firstSymbol) !== "string" || typeof (secondSymbol) !== "string") throw new Error("Envalid parameters, functions Wrap only accepts Strings Symbols--> Wrap(element {string}, secondOptionalElement {string})")
+
                 if (firstSymbol && !secondSymbol) {
+                    //ThrowError if is not a Symbol...
+                    if (!firstSymbol.match(regExp)) throw new Error("Only accepts Symbols.")
+
                     oneSymbol(firstSymbol);
                     return this;
                 } else if (firstSymbol && secondSymbol) {
+                    //ThrowError if is not a Symbol...
+                    if (!firstSymbol.match(regExp) || !secondSymbol.match(regExp)) throw new Error("Only accepts Symbols.")
+
                     thowSymbol(firstSymbol, secondSymbol);
                     return this;
                 }
-                throw new Error("No spected case");
+
+                throw new Error("Somthing wrong ...");
             } catch (err) {
-                console.error("Error!", err);
+                console.error(err);
                 return false;
             }
         },
-        toString(){
+        toString: function toString() {
             return word;
         }
     }
 }
+
+console.log("This test should be *{hola} -->", test("hola").wrap("{", "}").wrap("*").toString());
+console.log("This test should be $[*{hola}]$ -->", test("hola").wrap("{", "}").wrap("*").wrap("[", "]").wrap("$", "$").toString());
+console.log("This test should be Error Somthing Wrong  ... -->", test("hola").wrap("{", "}").wrap("*").wrap("[", "]").wrap().toString());
+console.log("This test should be Error Envalid parameters, functions Wrap only accepts Strings Symbols--> Wrap(element {string}, secondOptionalElement {string}) -->", test("hola").wrap("{", "}").wrap("*").wrap("[", "]").wrap(3).toString());
+console.log("This test should be Error Only accepts Symbols. -->", test("hola").wrap("{", "}").wrap("*").wrap("[", "]").wrap("h").toString());
+console.log("This test should be Error Only accepts Symbols. -->", test("hola").wrap("{", "}").wrap("*").wrap("[", "]").wrap("[", "a").toString());
