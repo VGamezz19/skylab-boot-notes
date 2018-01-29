69530518f5482612ed9f186df301947d9acd1e15
VGamezz19 <victor.gamez1596@gmail.com>
2018-01-29 10:00:33 +0100

    recursive reverseString

--- course/semana01/10_recursive-function/reverseString/scripts/main.js
+++ course/semana01/10_recursive-function/reverseString/scripts/main.js
@@ -1,4 +1,4 @@
-function reverseString(input, i) {
+function _reverseString(input, i) {
     var i = i ||  0;
     if (i < input.length) {
         var letter
@@ -8,4 +8,17 @@ function reverseString(input, i) {
     }
     return ""
 }
+console.log(`"hello" Should return ---> olleh `, reverseString('hello'));
+
+
+//La mejor forma es ... 
+
+function reverseString(str) {
+    if (str === '') {
+        return ''
+    } else {
+       return reverseString(str.slice(1)) + str.charAt(0)
+    } 
+}
+
 console.log(`"hello" Should return ---> olleh `, reverseString('hello'));
