e028d0c93cceccb8e23fef5e40bd22316c2d7545
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-04 19:33:58 +0100

    memochallenge

--- course/semana01/10_recursive-function/fibonacciRecursive/scripts/main.js
+++ course/semana01/10_recursive-function/fibonacciRecursive/scripts/main.js
@@ -1,5 +1,21 @@
-function fibonacci (n){
-    if(n === 0) return 0
-	if(n === 1) return 1
-    return fibonacci(n-1) + fibonacci(n-2)
+let contadorRect = 1
+function fibonacci(num) {
+    contadorRect++
+    if (num === 0) return 0
+    if (num === 1) return 1
+    return fibonacci(num - 1) + fibonacci(num - 2)
 }
+
+let contadorMemo = 1
+function fibonacciMemo(num, memoria = {}) {
+    contadorMemo++
+    if (memoria[num]) return memoria[num]
+    if (num === 0) return 0
+    if (num === 1) return 1
+    return memoria[num] = fibonacciMemo(num - 1, memoria) + fibonacciMemo(num - 2, memoria)
+}
+
+console.log(fibonacci(20))
+console.log(fibonacciMemo(20))
+console.log("Count from countRect", contadorRect)
+console.log("Count from countMemo", contadorMemo)
