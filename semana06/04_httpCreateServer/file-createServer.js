a790fc687897087e48de52bb0b089fecb0479d8f
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-20 20:46:38 +0100

    🗃 tree/master

--- semana06/04_httpCreateServer/file-createServer.js
+++ semana06/04_httpCreateServer/file-createServer.js
@@ -1,10 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 const server = http.createServer((req, res) => {
-    
-    res.sendDate()
     fs.createReadStream(file).pipe(res)
-    //fs.createWriteStream(file).wr
 })
 
 const port = process.argv[2]
