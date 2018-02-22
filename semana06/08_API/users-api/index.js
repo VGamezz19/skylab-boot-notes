130ef65c9f4b44599f22f75b69a257fccc085364
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-22 21:13:34 +0100

    api-user-middleware

--- semana06/08_API/users-api/index.js
+++ semana06/08_API/users-api/index.js
@@ -1,47 +1,88 @@
 require('dotenv').config()
 
 const express = require('express')
-
 const bodyParser = require('body-parser')
-
+const {
+    okMsg,
+    errMsg
+} = require('./response')
 const app = express()
 
 const users = []
 
-app.get('/api/users', (req, res) => res.json(ok('Users listing succeeded.', users.map(({ username }) => ({ username })))))
+app.get('/api/users', (req, res) => res.json(okMsg('Users listing succeeded.', users.map(({
+    username
+}) => ({
+    username
+})))))
 
-const jsonBodyParser = bodyParser.json()
+//const jsonBodyParser = bodyParser.json()
+app.use(bodyParser.urlencoded())
 
-app.post('/api/users', jsonBodyParser, (req, res) => {
-    const { username, password } = req.body
+app.post('/api/users', /*jsonBodyParser ,*/ (req, res) => {
+    const {
+        username,
+        password
+    } = req.body
 
-    if (!username || !password) return res.json(ko('User registration failed.', 'Invalid username and/or password.'))
+    if (!username || !password) return res.json(errMsg('User registration failed.', 'Invalid username and/or password.'))
 
-    const alreadyExists = users.some(user => user.username === username)
+    const userAlreadyExists = users.some(user => user.username === username)
 
-    if (alreadyExists) return res.json(ko('User registration failed.', 'Username already exists.'))
+    if (userAlreadyExists) return res.json(errMsg('User registration failed.', 'Username already exists.'))
 
-    users.push({ username, password })
+    users.push({
+        username,
+        password
+    })
 
-    res.json(ok('User registration succeeded.'))
+    res.json(okMsg('User registration succeeded.'))
 })
 
-function ok(message, data) {
-    const res = { status: 'OK', message }
+//================
+//Testing Middleware
+function checkUserInputed(req, res, next) {
+    const method = req.method
+    const userInputed = req.query.q || req.query.query
 
-    if (data) res.data = data
+    if (!userInputed) return res.json(errMsg(`User ${method} failed.`, 'You need input user name.'))
 
-    return res
+    if (method === 'PUT') {
+        if (!req.body.username) return res.json(errMsg('User update failed.', 'Invalid username'))
+    }
+
+    const userAlreadyExists = users.some(_user => _user.username === userInputed)
+
+    if (!userAlreadyExists) return res.json(errMsg(`User ${method} failed.`, 'Username didn\'t exists.'))
+
+    next()
 }
 
-function ko(message, error) {
-    const res = { status: 'KO', message }
+app.put('/api/users', checkUserInputed ,(req, res) => {
+    const userInputed = req.query.q || req.query.query
+    const {username} = req.body
 
-    if (error) res.error = error
+    users.forEach(_user => {
+        if (_user.username === userInputed) _user.username = username
 
-    return res
-}
+        return _user
+    })
 
+    res.json(okMsg('User updated.'))
+})
+
+app.delete('/api/users', checkUserInputed, (req, res) => {
+    const userInputed = req.query.q || req.query.query
+
+    users.map((_user, i) => {
+        if (_user.username === userInputed) users.splice(i, 1)
+
+        return _user
+    })
+
+    res.json(okMsg('User deleted.'))
+})
+
+// ========== ⚡️ ==========
 const port = process.env.PORT
-
 app.listen(port, () => console.log(`Users API running on port ${port}`))
