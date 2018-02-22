c86375b52c0c6765ed772a4e2ba429b43ed45ed4
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-22 16:21:36 +0100

    :star2: API-USER init :star:

--- semana06/07_init_Express/landing/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.argv[2]
-
-app.get('/', (req,res) => res.send('Hello, World!'))
-app.listen(port, () => console.log(`landing server running on port ${port}`))
