57112071272193a9203a736f8820f9f710768b09
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-02 09:53:23 +0100

    init MongoDB

---
+++ semana07/04_MongoDB/baby-conn-mongodb/src/index.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+const express = require('express')
+const { MongoClient } = require('mongodb')
+
+const app = express()
+
+app.set('view engine', 'pug')
+
+MongoClient.connect('mongodb://localhost:27017', (err, conn) => {
+    if (err) throw err
+
+    const db = conn.db('bootcamp')
+
+    app.get('/', (req, res) => {
+
+        db.collection('staff').find({ name: regex, email: regex}).toArray((err, data) => {
+            if (err) throw err
+            console.log(data)
+            res.json(data)
+        })
+
+       // res.render('index')
+        // const query = req.query.q || req.query.query || ''
+    
+        // const regex = new RegExp(query, 'i')
+
+        // db.collection('staff').find({ name: regex, email: regex}).toArray((err, data) => {
+        //     if (err) throw err
+
+        //     res.json(data)
+        // })
+    })
+
+    const port = process.env.PORT
+
+    app.listen(port, () => console.log(`server runnning on port ${port}`)) 
+
+    process.on('SIGINT', () => {
+        console.log('stopping server')
+
+        conn.close()
+
+        process.exit()
+    })
+})
