37df3cf66f3725697a6438d4c31e3010c22cbace
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-07 18:52:54 +0100

    rcJS-api-cli

--- semana08/01_mongoose/users-api-server/src/index.js
+++ semana08/01_mongoose/users-api-server/src/index.js
@@ -4,9 +4,13 @@ const express = require('express')
 
 const routes = require('./routes')
 
+const cors = require('cors')
+
 require('./db')
 
 const app = express()
+
+app.use(cors())
 
 app.use('/api', routes)
 
